b4531659351d7c1b1aa38eb3f04bc1afd97ec36c
Alistair Burrowes <alistair.burrowes@gmail.com>
2018-09-18 21:06:16 +1000

    Some demo bits for parser combinators

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,22 +1,58 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
-module Parser
-  ( parseCommand
-  ) where
+module Parser where
 
 import           Data.Functor               (($>))
 import           Data.Void                  (Void)
-import           Text.Megaparsec            (Parsec, parseMaybe, (<|>))
-import           Text.Megaparsec.Char       (char, space, string')
+import           Text.Megaparsec            (Parsec, parseMaybe, (<|>), some)
+import           Text.Megaparsec.Char       (char, space, string', alphaNumChar)
 import qualified Text.Megaparsec.Char.Lexer as L (decimal)
 
 import qualified Types                      as T
 
 type Parser = Parsec Void String
 
+data Email = Email
+  { eFirstPart :: String
+  , eDomain :: String
+  } deriving Show
+
+email :: String
+email = "alistair@email.com"
+
+emailParser :: Parser Email
+emailParser = do
+  firstPart <- some alphaNumChar
+  char '@'
+  domain <- some alphaNumChar
+  string' ".com"
+  pure $ Email firstPart domain
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 move :: Parser T.Command
 move = string' "MOVE" $> T.Move
+
+move' :: Parser T.Command
+move' = (\_ -> T.Move) <$> string' "MOVE"
+
+move'' :: Parser T.Command
+move'' = do
+  _ <- string' "MOVE"
+  pure T.Move
 
 left :: Parser T.Command
 left = string' "LEFT" $> T.TurnLeft
